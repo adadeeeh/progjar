d0be04df764005c13e0fd5dd419af4be734e26fa
Faturrahman Ma'ruf <faturrahmanmakruf@gmail.com>
2017-10-09 17:50:23 +0700

    File kecil udah bisa, file besar masih error

--- 5115100027_5115100082/client/client.py
+++ 5115100027_5115100082/client/client.py
@@ -1,40 +1,24 @@
 import socket
 import sys
-import os
 
 server_address = ('127.0.0.1', 5000)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
-sys.stdout.write('>>')
+nama_file = raw_input()
+nama_file = nama_file.split(" ", 1)
 
-nama_file = sys.stdin.readline()
-nama_file = nama_file.replace("\n", "")
-nama_file_split = nama_file.split(" ")
-#print nama_file_split
-while nama_file_split[0] != 'unduh':
-    print "Perintah yang anda masukkan salah"
-    client_socket.send(nama_file)
-    break
-file_ada = 0
-for file in os.listdir("/home/pedro/Documents/python-networking/5115100027_5115100082/server/dataset/"):
-    if file == nama_file_split[1]:
-        file_ada = 1
-        break
-
-if file_ada == 1:
-    client_socket.send(nama_file)
+while True:
+    client_socket.send(nama_file[1])
     tmp = client_socket.recv(1024)
-    #with open("downloads/"+nama_file_split[1], "wb") as file:
-    #    file.write(tmp)
-    down_file = open("downloads/"+nama_file_split[1], "wb")
-    down_file.write(tmp)
+    print tmp
+    new_tmp = tmp.splitlines(True)
+    down_file = open("downloads/"+nama_file[1], "wb")
     while tmp:
-        down_file.write(tmp)
+        print new_tmp[0]
+        down_file.writelines(new_tmp[1:])
         tmp = client_socket.recv(1024)
-    print "unduh selesai"
-else:
-    print nama_file_split[1]+" tidak ketemu"
+    break
 
 client_socket.close()
 sys.exit(0)
--- 5115100027_5115100082/server/server.py
+++ 5115100027_5115100082/server/server.py
@@ -1,57 +1,43 @@
 import socket
 import select
-import os
 import sys
-import StringIO
+import os
 
-server_address = ('127.0.0.1', 5000)
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+server_address = ('127.0.0.1',5000)
+server_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
 server_socket.bind(server_address)
 server_socket.listen(5)
-#input_socket = [server_socket]
 
-while True:
-    conn, addr = server_socket.accept()
-    nama_file = conn.recv(1024)
-    if nama_file:
-        nama_file = nama_file.split(" ")
-        #print nama_file
-        if nama_file[0] != "unduh":
-            print "Perintah yang dimasukkan salah"
-        #break
-        file_ketemu = 0
-        for file in os.listdir("dataset/"):
-            if file == nama_file[1]:
-                file_ketemu = 1
-                #break
+input_socket = [server_socket]
 
-        if file_ketemu == 0:
-            print nama_file[1] + " tidak ada di server"
+try:
+   while True:
+         read_ready,write_ready,exception = select.select(input_socket,[],[])
 
-        else:
-            print "unduh " + nama_file[1] + " dari alien"
-            #with open("dataset/" + nama_file[1], "rb") as file:
-            #    up_file = file.read()
-            up_file = open("dataset/" + nama_file[1], "rb")
-            #up_file = file.read()
-            header = "nama_file: " + nama_file[1] + '\n' + "file_size: " + str(os.path.getsize('/home/pedro/Documents/python-networking/5115100027_5115100082/server/dataset/' + nama_file[1])) + '\n' + '\n\n'
-            print header
-            #data = header + nama_file[1]
-            #print data
-            # data = data.split('\n')
-            # print data
-            # data1 = [StringIO.StringIO(x) for x in data]
-            #read_file = data1.read(2048)
-            #sock.sendall(up_file)
-            read_file = up_file.read(1024)
-            while read_file:
-                conn.send(read_file)
-                read_file = up_file.read(1024)
-    else:
-        conn.close()
-        #input_socket.remove(conn)
-    break
+         for sock in read_ready:
+            if sock ==server_socket:
+               client_socket,client_address = server_socket.accept()
+               input_socket.append(client_socket)
 
-server_socket.close()
-sys.exit(0)
+            else:
+               file = sock.recv(1024)
+               if file:
+                  file2 = file.replace("\n","")
+                  path = 'dataset/'+file2
+                  print "unduh "+file2+" dari alien"
+                  with open(path, 'rb') as file:
+                     up_file = file.read()
+                  header = "name: "+file2+"\n"
+                  print up_file
+                  paket = header+up_file
+                  print paket
+                  sock.sendall(paket)
+               else:
+                  sock.close()
+                  input_socket.remove(sock)
+
+except KeyboardInterrupt:
+   server_socket.close
+   sys.exit(0)
