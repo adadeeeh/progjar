9954a899e342324b529373263b9b2690f384025e
Faturrahman Ma'ruf <faturrahmanmakruf@gmail.com>
2017-10-09 10:10:48 +0700

    Ubah server dan client

--- 5115100027_5115100082/client/client.py
+++ 5115100027_5115100082/client/client.py
@@ -1,21 +1,32 @@
 import socket
 import sys
+import os
 
 server_address = ('127.0.0.1', 5000)
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client_socket.connect(server_address)
 
 nama_file = raw_input()
-nama_file = nama_file.split(" ")
+nama_file_split = nama_file.split(" ")
+while nama_file_split[0] != 'unduh':
+    print "Perintah yang anda masukkan salah"
+    client_socket.send(nama_file)
+    break
+file_ada = 0
+for file in os.listdir("/home/pedro/Documents/python-networking/5115100027_5115100082/server/dataset/"):
+    if file == nama_file_split[1]:
+        file_ada = 1
+        break
 
-while True:
-    client_socket.send(nama_file[1])
-    tmp = client_socket.recv(1024)
-    down_file = open("downloads/"+nama_file[1], "wb")
+if file_ada == 1:
+    client_socket.send(nama_file)
+    tmp = client_socket.recv(2048)
+    down_file = open("downloads/"+nama_file_split[1], "wb")
     while tmp:
         down_file.write(tmp)
-        tmp = client_socket.recv(1024)
-    break
+        tmp = client_socket.recv(2048)
+    print "unduh selesai"
+else:
+    print nama_file_split[1]+" tidak ketemu"
 
 client_socket.close()
-sys.exit(0)
--- 5115100027_5115100082/server/server.py
+++ 5115100027_5115100082/server/server.py
@@ -2,6 +2,7 @@ import socket
 import select
 import os
 import sys
+import StringIO
 
 server_address = ('127.0.0.1', 5000)
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -9,27 +10,42 @@ server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 server_socket.bind(server_address)
 server_socket.listen(5)
 
+file_ketemu = 0
+
+c = "/home/pedro/Documents/python-networking/5115100027_5115100082/server/dataset"
+
+
 while True:
     conn, addr = server_socket.accept()
     nama_file = conn.recv(1024)
+    nama_file = nama_file.split(" ")
+    if nama_file[0] != "unduh":
+        print "Perintah yang dimasukkan salah"
+        break
     for file in os.listdir("dataset/"):
-        if file == nama_file:
+        if file == nama_file[1]:
             file_ketemu = 1
             break
 
     if file_ketemu == 0:
-        file_ketemu = 0
-        print nama_file+" tidak ada di server"
+        print nama_file[1]+" tidak ada di server"
 
     else:
-        print "unduh "+nama_file+" dari alien"
-        up_file = open("dataset/"+nama_file, "rb")
-        read_file = up_file.read(1024)
+        print "unduh "+nama_file[1]+" dari alien"
+        up_file = open("dataset/"+nama_file[1], "rb")
+            #up_file = file.read()
+        header = "nama_file: "+nama_file[1] + '\n'+"file_size: "+str(os.path.getsize('/home/pedro/Documents/python-networking/5115100027_5115100082/server/dataset/'+nama_file[1]))+'\n'+'\n\n'
+        #print header
+        data = header+nama_file[1]
+        print data
+        #data = data.split('\n')
+        #print data
+        #data1 = [StringIO.StringIO(x) for x in data]
+        #read_file = data1.read(2048)
+        read_file = up_file.read(2048)
         while read_file:
             conn.send(read_file)
-            read_file = up_file.read(1024)
+            read_file = up_file.read(2048)
     break
 
 server_socket.close()
-server_socket.close()
-sys.exit(0)
